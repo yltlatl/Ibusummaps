7e6e471569b6ce6c0cba9a27f189d825c18851ae
Aaron Gardner <aaron.gardner@yahoo.com>
2016-03-30 07:31:49 -0400

    Blank project

---
+++ Ibusummaps/Ibusummaps/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ibusummaps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
